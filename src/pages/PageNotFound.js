9ea7aca7bfbfdc6eb8b9a69992a41466bbafabe9
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-09-19 12:20:30 -0500

    use Navigate compoennt to navigate based of URL

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom'
 
 export default function PageNotFound() {
   return (
-    <div>PageNotFound</div>
+    <Navigate to="/"/>
   )
 }
